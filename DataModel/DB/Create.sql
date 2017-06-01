55357205d7f888f11afa2cf77a2a2ca4e2c839a5
NAlex2004 <nalex2004@gmail.com>
2017-06-01 15:05:38 +0300

    .

--- DataModel/DB/Create.sql
+++ DataModel/DB/Create.sql
@@ -6,8 +6,7 @@ on
 log on
 ( NAME = Sales_log,
   FILENAME = 'E:\My\Private\CS\Training\Selling\DataModel\DB\Sales.ldf')
-  
-*/
+*/  
 
 use Sales
 go
@@ -25,14 +24,28 @@ create table Products
 	Price float not null default 0
 )
 
+
+create table Managers
+(
+	Id int not null identity primary key,
+	LastName varchar(100) not null unique,	
+)
+
 create table Sales
 (
+	SaleDate datetime not null,
+	ManagerId int not null,
 	CustomerId int not null,
 	ProductId int not null,
 	Total float not null default 0,
 	constraint FK_Customer foreign key (CustomerID) references Customers(Id),
-	constraint FK_Product foreign key (ProductID) references Products(Id)
+	constraint FK_Product foreign key (ProductID) references Products(Id),
+	constraint FK_Manager foreign key (ManagerId) references Managers(Id)
 )
+
 
 create index Ind_CustomerName on Customers(CustomerName)
 create unique index Ind_ProductName on Products(ProductName)
+
+
+
---
+++ DataModel/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace NAlex.DataModel.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
